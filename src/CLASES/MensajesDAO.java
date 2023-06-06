4113dd069312c58578c99b42a01d5885ca9813cb
David Giraldo <santiagogiraldosuescun@gmail.com>
2023-06-06 17:04:38 -0500

    Seguardan las nuevas clases, DAO, Servicios y Mensajes

---
+++ src/CLASES/MensajesDAO.java
@@ -0,0 +1,26 @@
+
+package CLASES;
+//En esta clase se crean los metodos para hacer un CRUD, crear leer actualizar y borrar
+public class MensajesDAO {
+    
+    //Metodo para guardar un mensaje en la base de datos
+    public void crearMensaje(Mensajes mensaje){
+        
+    }//Cierre del metodo 
+    
+    //Metodo para ver las tuplas de la base de datos
+    public void leerMensajes(){
+        
+    }//Cierre del metodo
+    
+    //Metodo para eliminar los mensajes
+    public void borrarMensaje(int id_mensaje){
+        
+    }//Cierre del metodo
+    
+    //Metodo para actualizar la base de datos
+    public void actualizarMensaje(int id_mensaje){
+        
+    }//Cierre del metodo
+    
+}
---
+++ src/CLASES/mensajesService.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CLASES;
+
+//En esta clase se crea el menu que se conecta con la clase MensajesDAO
+public class mensajesService {
+    //Metodo para ingresar el mensaje
+    public static void crearMensaje(){
+        
+    }//Cierre del metodo 
+    
+    //Metodo para listar los mensajes que esten en la base de datos
+    public static void listarMensajes(){
+        
+    }//Cierre del metodo
+    
+    //Metodo para borrar un mensaje de la base de datos
+    public static void borrarMensajes(){
+        
+    }//Cierre del metodo
+    
+    //Metodo para actualizar un mensaje
+    public static void editarMensaje(){
+        
+    }//Cierre del metodo
+    
+}
