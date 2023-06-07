62748dc8f4f203acb32504ba3e8b30429e45fed4
David Giraldo <santiagogiraldosuescun@gmail.com>
2023-06-06 19:05:07 -0500

    Haciendo que funcione

--- src/CLASES/Conexion.java
+++ src/CLASES/Conexion.java
@@ -1,25 +1,64 @@
 package CLASES;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
 public class Conexion {
    //crear la variable de conexion
-    Connection con=null;
+    static Connection con=null;
     
     //Crear las variables para la cadena de conexion    
     private String url="jdbc:sqlserver://localhost:1433;database=MensajesApp;integratedSecurity=true;";   
-    private String SQL="";
+    private static String SQL = "";
     
     //Método para establecer la conexion
     public Connection getConexion(){
         try{
             con=DriverManager.getConnection(url);
+            if (con!=null) {
+                JOptionPane.showMessageDialog(null, "Conectado");
+            }
         }catch(SQLException ex){
             JOptionPane.showMessageDialog(null, "ERROR: "+ex.getMessage());
         }
         return con;
     }//Cierre del método 
+
+    //Metodo para guardar un mensaje en la base de datos
+    public  void crearMensaje(String Mensaje, String Nombre) {
+       try{
+            SQL="INSERT INTO Mensaje VALUES(?,?)";
+            //Crear la sentencia
+            PreparedStatement ps=con.prepareStatement(SQL);
+            ps.setString(1, Mensaje);
+            ps.setString( 2,Nombre);
+            
+            //Ejecutar la instruccion SQL
+            ps.executeUpdate();
+            System.out.println("Mensaje guardado");
+            ps.close();
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "Error: " + e.getMessage());
+        }
+
+        
+    }//Cierre del metodo 
+
+    //Metodo para ver las tuplas de la base de datos
+    public  void leerMensajes() {
+
+    }//Cierre del metodo
+
+    //Metodo para eliminar los mensajes
+    public  void borrarMensaje(int id_mensaje) {
+
+    }//Cierre del metodo
+
+    //Metodo para actualizar la base de datos
+    public  void actualizarMensaje(int id_mensaje) {
+
+    }//Cierre del metodo
+
+   
 }
--- src/CLASES/mensajesService.java
+++ src/CLASES/mensajesService.java
@@ -6,41 +6,37 @@
 package CLASES;
 
 //En esta clase se crea el menu que se conecta con la clase MensajesDAO
-
 import java.util.Scanner;
 
 public class mensajesService {
+
     //Metodo para ingresar el mensaje
-    public static void crearMensaje(){
+    public static void crearMensaje() {
         Scanner sc = new Scanner(System.in);
         
         System.out.println("Escribe tu mensaje: ");
         String Mensaje = sc.nextLine();
-        
+
         System.out.println("Tu nombre: ");
         String Nombre = sc.nextLine();
-        
-        Mensajes registro = new Mensajes();
-        
-        registro.setMensaje(Mensaje);
-        registro.setAutor_mensaje(Nombre);
-        
-        MensajesDAO.crearMensaje(registro);
+        Conexion con = new Conexion();
+        con.getConexion();
+        con.crearMensaje(Mensaje, Nombre);
     }//Cierre del metodo 
-    
+
     //Metodo para listar los mensajes que esten en la base de datos
-    public static void listarMensajes(){
-        
+    public static void listarMensajes() {
+
     }//Cierre del metodo
-    
+
     //Metodo para borrar un mensaje de la base de datos
-    public static void borrarMensajes(){
-        
+    public static void borrarMensajes() {
+
     }//Cierre del metodo
-    
+
     //Metodo para actualizar un mensaje
-    public static void editarMensaje(){
-        
+    public static void editarMensaje() {
+
     }//Cierre del metodo
-    
+
 }
